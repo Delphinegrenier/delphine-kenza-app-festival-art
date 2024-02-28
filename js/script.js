c602cb17787a48aa5e7009e4da514ce915bd8eaa
Delphine <delphinegren@gmail.com>
2024-02-27 23:17:51 -0500

    Ajout swiper javascript

--- js/script.js
+++ js/script.js
@@ -1,4 +1,24 @@
 var swiper = new Swiper(".swiper-filters", {
-    slidesPerView: 'auto',
-    freeMode: true,
-  });
+  slidesPerView: 'auto',
+  freeMode: true,
+});
+
+var swiperScreenshots = new Swiper(".swiper-screenshots", {
+  // Default parameters
+  slidesPerView: 1,
+  spaceBetween: 10,
+  loop: true,
+  centeredSlides: true,
+  // Responsive breakpoints
+  breakpoints: {
+    // when window width is >= 320px
+    1200: {
+      slidesPerView: 3,
+      spaceBetween: 50,
+    },
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
