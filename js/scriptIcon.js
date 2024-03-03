8c222d832f461e26601376d39ab422126a15245b
KenzaElHarrif <112128295+KenzaElHarrif@users.noreply.github.com>
2024-03-02 21:14:16 -0500

    peaufinage js

--- js/scriptIcon.js
+++ js/scriptIcon.js
@@ -3,14 +3,11 @@ var isActive = true;
 
 btn.addEventListener('click', () => {
   isActive=! isActive;
-  console.log("hello")
   if(isActive){
-    console.log('test if')
     btn.classList.add('text-light');
     btn.classList.remove('text-secondary');
   }
   else{
-    console.log('test else')
     btn.classList.add('text-secondary');
     btn.classList.remove('text-light');
   }
