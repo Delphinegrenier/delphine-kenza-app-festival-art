66dfb8944d2fb8e65e87255a51fd06cb73f63f67
KenzaElHarrif <112128295+KenzaElHarrif@users.noreply.github.com>
2024-03-12 22:01:33 -0400

    derniers changements css html

--- js/scriptIcon.js
+++ js/scriptIcon.js
@@ -4,11 +4,15 @@ var isActive = true;
 btn.addEventListener('click', () => {
   isActive=! isActive;
   if(isActive){
-    btn.classList.add('text-light');
+    btn.classList.add('text-dark');
     btn.classList.remove('text-secondary');
+    btn.classList.add('bg-secondary');
+    btn.classList.remove('bg-primary');
   }
   else{
     btn.classList.add('text-secondary');
     btn.classList.remove('text-light');
+    btn.classList.add('bg-primary');
+    btn.classList.remove('bg-secondary');
   }
 })
